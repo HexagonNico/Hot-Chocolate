e94f570b80812e286ba84e2683074c919fc4fe46
Nico <nico.hex6@gmail.com>
2022-03-07 13:44:24 +0100

    Chocolate bar food

--- src/main/java/hexagonnico/hotchocolate/registry/ModItems.java
+++ src/main/java/hexagonnico/hotchocolate/registry/ModItems.java
@@ -13,6 +13,7 @@ public final class ModItems {
 	
 	public static final DeferredRegister<Item> REGISTER = DeferredRegister.create(ForgeRegistries.ITEMS, HotChocolate.ID);
 
+	public static final RegistryObject<Item> CHOCOLATE_BAR = REGISTER.register("chocolate_bar", () -> new Item(new Item.Properties().food(ModFood.CHOCOLATE_BAR).tab(CreativeModeTab.TAB_FOOD)));
 	public static final RegistryObject<Item> CHOCOLATE_MILK = REGISTER.register("chocolate_milk", () -> new ChocolateMilkItem(new Item.Properties().food(ModFood.CHOCOLATE_MILK).craftRemainder(Items.BUCKET).stacksTo(1).tab(CreativeModeTab.TAB_MISC)));
 	public static final RegistryObject<Item> HOT_CHOCOLATE = REGISTER.register("hot_chocolate", () -> new ChocolateMilkItem(new Item.Properties().food(ModFood.HOT_CHOCOLATE).craftRemainder(Items.BUCKET).stacksTo(1).tab(CreativeModeTab.TAB_MISC)));
 }
